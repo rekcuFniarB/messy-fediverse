bf64ecc3113cf396bcecfd850873675630ad031e
rekcuFniarB <retratserif@gmail.com>
2022-05-16 02:39:07 +0300

    Fediverse: trying to fix digest computing.

--- src/messy_fediverse/fediverse.py
+++ src/messy_fediverse/fediverse.py
@@ -78,8 +78,10 @@ class Fediverse:
                 if 'headers' not in kwargs:
                     kwargs['headers'] = {}
                 request_date = emailutils.format_datetime(datetime.fromisoformat(kwargs['json']['object']['published'])).replace(' -0000', ' GMT')
+                kwargs['data'] = json.dumps(kwargs['json'])
+                del(kwargs['json'])
                 kwargs['headers']['Date'] = request_date
-                kwargs['headers']['Digest'] = 'sha-256=' + sha256(json.dumps(kwargs['json']).encode('utf-8')).hexdigest()
+                kwargs['headers']['Digest'] = 'sha-256=' + sha256(kwargs['data'].encode('utf-8')).hexdigest()
                 kwargs['headers']['Signature'] = self.sign(url, kwargs['headers'])
         
         r = requests.post(url, *args, **kwargs)

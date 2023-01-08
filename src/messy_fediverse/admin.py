045ddb5d0f2ccc84954ea9c309bda34a7f0bf2bf
rekcuFniarB <retratserif@gmail.com>
2023-01-09 01:18:28 +0300

    Fixing follow accept requests.

--- src/messy_fediverse/admin.py
+++ src/messy_fediverse/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import FederatedEndpoint, Activity, Follower
 from .controller import fediverse_factory, stderrlog
 from asgiref.sync import sync_to_async, async_to_sync
+import aiohttp
+# import asyncio
 
 class FollowerAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
@@ -13,38 +15,56 @@ class FollowerAdmin(admin.ModelAdmin):
         ## obj._loaded_values provided by model's custom from_db()
         if not form.cleaned_data['disabled'] and form.cleaned_data['accepted']:
             if not obj._loaded_values['accepted']:
-                result = async_to_sync(self.send_accept_follow, force_new_loop=True)(request, obj)
+                fediverse = fediverse_factory(request)
+                try:
+                    result = async_to_sync(self.send_accept_follow)(fediverse, obj)
+                except BaseException as e:
+                    # if e.args[0] == 'Event loop is closed':
+                    #     loop = asyncio.new_event_loop()
+                    #     stderrlog('NEW LOOP:', loop)
+                    #     result = async_to_sync(self.send_accept_follow)(fediverse, obj)
+                    #     # result = asyncio.run(self.send_accept_follow(request, obj))
+                    # else:
+                    #     raise
+                    raise
+                
+                # result = asyncio.run(self.send_accept_follow(fediverse, obj))
+                stderrlog('SAVE RESULT:', result)
+                ## FIXME check response and discard "accepted" if request failed.
         
         return super().save_model(request, obj, form, change)
     
-    async def send_accept_follow(self, request, follower):
-        fediverse = fediverse_factory(request)
-        session = await fediverse.http_session()
+    async def send_accept_follow(self, fediverse, follower):
+        response = None
+        async with aiohttp.ClientSession() as session:
+            #fediverse = fediverse_factory(request)
+            # session = await fediverse.http_session()
+            await fediverse.http_session(session)
+            
+            ## FIXME succeedes only once, second time it returns "loop closed" error
+            actorInfo, = await fediverse.gather_http_responses(
+                fediverse.get(follower.uri, session=session)
+            )
+            
+            if type(actorInfo) is not dict:
+                if isinstance(actorInfo, BaseException):
+                    raise actorInfo
+                elif type(actorInfo) is str:
+                    raise TypeError(actorInfo)
+                else:
+                    raise TypeError('Bad actor info:', type(actorInfo), str(actorInfo))
+            
+            ## Getting activityPub object dict
+            apobject = follower.activity.get_dict().get('object', None)
+            acceptActivity = fediverse.activity(
+                type='Accept',
+                object=apobject,
+                to=[actorInfo['id']]
+            )
+            response, = await fediverse.gather_http_responses(
+                fediverse.post(actorInfo['endpoints']['sharedInbox'], session, json=acceptActivity)
+            )
         
-        ## FIXME succeedes only once, second time it returns "loop closed" error
-        actorInfo, = await fediverse.gather_http_responses(
-            fediverse.get(follower.uri, session=session)
-        )
-        
-        if type(actorInfo) is not dict:
-            if isinstance(actorInfo, BaseException):
-                raise actorInfo
-            elif type(actorInfo) is str:
-                raise TypeError(actorInfo)
-            else:
-                raise TypeError('Bad actor info:', type(actorInfo), str(actorInfo))
-        
-        ## Getting activityPub object dict
-        apobject = follower.activity.get_dict().get('object', None)
-        acceptActivity = fediverse.activity(
-            type='Accept',
-            object=apobject,
-            to=[actorInfo['id']]
-        )
-        response, = await fediverse.gather_http_responses(
-            fediverse.post(actorInfo['endpoints']['sharedInbox'], session, json=acceptActivity)
-        )
-        stderrlog('RESPONSE:', response, '\nACTIVITY:', acceptActivity)
         return response
     
 admin.site.register(FederatedEndpoint)

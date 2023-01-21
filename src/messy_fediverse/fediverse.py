rekcuFniarB/messy-fediverse

commit 2e9cc6c7a0e6aa15b154b1e5711c36b4f8457916

ActivityPub instance may not have sharedInbox, at least Honk.

src/messy_fediverse/fediverse.py
@@ -434,16 +434,29 @@ class Fediverse:
         results = await self.gather_http_responses(*results)
         
         for user in results:
+            endpoint = None
             if type(user) is dict and 'endpoints' in user and 'sharedInbox' in user['endpoints']:
-                if user['endpoints']['sharedInbox'] not in endpoints:
-                    endpoints.append(user['endpoints']['sharedInbox'])
+                endpoint = user['endpoints']['sharedInbox']
+            elif 'inbox' in user:
+                ## I saw instances without sharedInbox, at least Honk
+                endpoint = user['inbox']
+            
+            if type(endpoint) is list and len(endpoint) > 0:
+                ## Never saw such case but anyway...
+                endpoint = endpoint[0]
+            else:
+                endpoint = None
+            
+            if endpoint:
+                if endpoint not in endpoints:
+                    endpoints.append(endpoint)
                 if user['id'] not in activity['object']['cc'] and user['id'] not in activity['object']['to']:
                     activity['object']['cc'].append(user['id'])
         
         activity['cc'] = activity['object']['cc']
         
         results = []
-        activity['object']['mentionResults'] = []
+        activity['mentionResults'] = []
         
         for endpoint in endpoints:
             start_ts = datetime.now().timestamp()
@@ -457,10 +470,10 @@ class Fediverse:
         self.stderrlog('REQUESTS GATHER TS:', diff_ts)
         
         for n, result in enumerate(results):
-            activity['object']['mentionResults'].append((endpoints[n], result))
+            activity['mentionResults'].append((endpoints[n], result))
         
         ## For debug
-        activity['object']['mentionEndpoints'] = endpoints
+        activity['mentionEndpoints'] = endpoints
         
         return results
     

src/messy_fediverse/controller.py
@@ -784,8 +784,10 @@ def status(request, rpath):
     with open(filepath, 'rt', encoding='utf-8') as f:
         data = json.load(f)
     
-    if 'object' in data and type(data['object']) is dict:
-        data = data['object']
+    activity = data
+    
+    if 'object' in activity and type(activity['object']) is dict:
+        data = activity['object']
     
     if 'conversation' not in data and 'context' not in data:
         data['context'] = data['conversation'] = data['id']
@@ -800,7 +802,7 @@ def status(request, rpath):
         return ActivityResponse(data, request)
     
     if request.user.is_staff:
-        data['raw_json'] = json.dumps(data, indent=4)
+        data['raw_json'] = json.dumps(activity, indent=4)
     elif 'inReplyTo' in data and data['inReplyTo']:
         return redirect(data['inReplyTo'])
     elif 'url' in data and data['url'] and data['url'] != data['id']:

commit 2abd8d94e122badfb5e6b2f4166afdce4fbba33e

Fix: mention() (broken in 2e9cc6c7a0e6aa15b)

src/messy_fediverse/fediverse.py
@@ -444,8 +444,6 @@ class Fediverse:
             if type(endpoint) is list and len(endpoint) > 0:
                 ## Never saw such case but anyway...
                 endpoint = endpoint[0]
-            else:
-                endpoint = None
             
             if endpoint:
                 if endpoint not in endpoints:
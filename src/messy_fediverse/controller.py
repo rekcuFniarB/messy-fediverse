fa4fb0af34d77af0cb557bfcb5656c76355adb76
Pyroman <pyroman@ofthe.top>
2023-02-05 19:53:21 +0300

    Fixing error context cannot be null

--- src/messy_fediverse/controller.py
+++ src/messy_fediverse/controller.py
@@ -400,7 +400,7 @@ async def save_activity(request, activity):
                 activity_type = actType,
                 actor_uri = activity.get('actor', None),
                 object_uri = object_id,
-                context = apobject.get('context', apobject.get('conversation', activity.get('context', activity.get('conversation', apobject.get('id'))))),
+                context = apobject.get('context', apobject.get('conversation', activity.get('context', activity.get('conversation', apobject.get('id', ''))))),
                 incoming = incoming
             )
             

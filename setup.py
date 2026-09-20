a34f172589164f174a8da701af985c733805c02d
rekcuFniarB <retratserif@gmail.com>
2026-09-21 01:33:07 +0300

    Fix migrations place.

--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ setuptools.setup(
         'messy_fediverse.static.messy.fediverse',
         'messy_fediverse.templates',
         'messy_fediverse.templates.messy.fediverse',
-        'messy_fediverse.management.commands'
+        'messy_fediverse.management.commands',
+        'messy_fediverse.migrations',
     ],
     package_data={
         '': ['*.conf', '*.md', '*.txt', '*.html', '*.js', '*.css', '*.json', '*.jsonld']
